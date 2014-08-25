035b89c6dd23424f56eb37b73394e35f2c18b40a
Jeff Risberg <jrisberg@aol.com>
2014-08-25 23:11:57 +0000

    filled out more crud operations

--- api/controllers/ActivityController.js
+++ api/controllers/ActivityController.js
@@ -7,9 +7,7 @@ module.exports = {
                 return;
             }
 
-            var data = {activities: activities};
-
-            res.view(data);
+            res.view({activities: activities});
         });
     },
 
@@ -17,15 +15,17 @@ module.exports = {
         var id = req.param("id", null);
 
         Activity.findOne(id, function(err, activity) {
-            var data = { activity: activity };
-
-            console.log("found the activity " + activity);
             if (activity === undefined) return res.notFound();
-            res.view(data);
+            res.view({activity, activity});
         });
     },
 
     create: function (req, res) {
+      var activity = new Activity();
+      res.view({activity: activity});
+    },
+
+    save: function (req, res) {
         Activity.create({name: req.param("name"), 
                          type: req.param("type"), 
                          points: req.param("points")}, 
@@ -33,5 +33,20 @@ module.exports = {
 
             res.redirect("/activity");
         });
+    },
+
+    edit: function (req, res) {
+        var id = req.param("id", null);
+  
+        Activity.findOne(id, function(err, activity) {
+            if (activity === undefined) return res.notFound();
+            res.view({activity: activity});
+        });
+    },
+
+    update : function (req, res) {
+    },
+
+    delete: function (req, res) {
     }     
 };
