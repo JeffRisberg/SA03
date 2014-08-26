6417adb321ef529b4759900215bf5c765f1be45e
Jeff Risberg <jrisberg@aol.com>
2014-08-26 00:28:48 +0000

    fix

--- api/controllers/ActivityController.js
+++ api/controllers/ActivityController.js
@@ -1,29 +1,29 @@
 module.exports = {
     index: function (req, res) {      
-        Activity.find({}).exec(function(err, activities) {
-            if (err) {
-                sails.log.err("-----------err", err);
-                res.send(400);
-                return;
-            }
+      Activity.find({}).exec(function(err, activities) {
+        if (err) {
+          sails.log.err("-----------err", err);
+          res.send(400);
+          return;
+        }
 
-            res.view({activities: activities});
-        });
+        res.view({activities: activities});
+      });
     },
 
     view: function (req, res) {
-        var id = req.param("id", null);
+      var id = req.param("id", null);
 
-        Activity.findOne(id, function(err, activity) {
-            if (activity === undefined) return res.notFound();
+      Activity.findOne(id, function(err, activity) {
+        if (activity === undefined) return res.notFound();
 
-            res.view({activity: activity});
-        });
+        res.view({activity: activity});
+      });
     },
 
     create: function (req, res) {
       if (req.method == "POST" && req.param("Activity", null) != null) {
-        Activity.create(req.param("Activity")).done(function (err, model) {
+        Activity.create(req.param("Activity"), function (err, model) {
 
           if (err) {
             res.send("Error something went wrong");
@@ -39,8 +39,9 @@ module.exports = {
     update: function (req, res) {
       var id = req.param('id', null);
     
-      Activity.findOne({id: id}).done(function(err, activity) {
-            
+      Activity.findOne(id, function(err, activity) {
+        if (activity === undefined) return res.notFound();
+
         if (req.method = "POST" && req.param('Activity', null) != null) {
           var p = req.param('Activity', null);
                       
@@ -64,10 +65,11 @@ module.exports = {
     delete: function (req, res) {
       var id = req.param('id', null);
 
-      Activity.findOne({id: id}).done(function(err, activity) {
+      Activity.findOne(id, function(err, activity) {
+        if (activity === undefined) return res.notFound();
+
         activity.destroy(function (error) {
-          res.redirect('/activity/index');
-        });
+        res.redirect('/activity/index');       
       });          
     }     
 };
