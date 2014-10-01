a9bf0a43372ed4325f88b7bf1917896815c1bb2e
Jeff Risberg <jrisberg@aol.com>
2014-10-01 09:56:20 -0700

    migrated to building on IntelliJ

--- api/models/Activity.js
+++ api/models/Activity.js
@@ -1,30 +1,30 @@
 /**
-* Activity.js
-*
-* @description :: each Activity has id, name, points
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * Activity.js
+ *
+ * @description :: each Activity has id, name, points
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
 
 module.exports = {
 
-  attributes: {
-     "id": {
-        "type": "integer",
-        "primaryKey": true,
-        "autoIncrement": true,
-        "columnName": "ID"
-     },
-     "name": {
-        "type": "string",
-        "maxLength": 35
-     },
-     "type": {
-        "type": "string",
-        "maxLength": 20
-     },
-     "points": {
-        "type": "integer"
-     }
-  }
+    attributes: {
+        "id": {
+            "type": "integer",
+            "primaryKey": true,
+            "autoIncrement": true,
+            "columnName": "id"
+        },
+        "name": {
+            "type": "string",
+            "maxLength": 35
+        },
+        "type": {
+            "type": "string",
+            "maxLength": 20
+        },
+        "points": {
+            "type": "integer"
+        }
+    }
 };
 
--- config/connections.js
+++ config/connections.js
@@ -2,7 +2,6 @@
  * Connections
  * (sails.config.connections)
  *
- * .
  * Note: If you're using version control, you should put your passwords/api keys
  * in `config/local.js`, environment variables, or use another strategy.
  * (this is to prevent you inadvertently sensitive credentials up to your repository.)
@@ -11,33 +10,39 @@
 
 module.exports.connections = {
 
+    /***************************************************************************
+     *                                                                          *
+     * PostgreSQL is another officially supported relational database.          *
+     * http://en.wikipedia.org/wiki/PostgreSQL                                  *
+     *                                                                          *
+     * Run: npm install sails-postgresql                                        *
+     *                                                                          *
+     ***************************************************************************/
+    postgres1: {
+        module: 'sails-postgresql',
+        //host: 'localhost',
+        //port: 5432,
+        //user: 'postgres',
+        //password: '123456',
+        //database: 'sa02',
+        url: 'jdbc:postgresql://localhost/sa02?user=postgres&password=123456',
 
-  /***************************************************************************
-  *                                                                          *
-  * PostgreSQL is another officially supported relational database.          *
-  * http://en.wikipedia.org/wiki/PostgreSQL                                  *
-  *                                                                          *
-  * Run: npm install sails-postgresql                                        *
-  *                                                                          *
-  *                                                                          *
-  ***************************************************************************/
-  postgres1: {
-    adapter: 'sails-postgresql',
-    //host:     process.env.PG_HOSTNAME || 'localhost',
-    //user:     process.env.PG_USER     || 'root',
-    //password: process.env.PG_PASSWORD || '',
-    //database: process.env.PG_DATABASE || 'SA02',
-    //port:     process.env.PG_PORT     || 5532,
-    url:      process.env.DATABASE_URL || '',
-    pool: false,
-    ssl: true
-  }
+        //adapter: 'sails-postgresql',
+        //host:     process.env.PG_HOSTNAME || 'localhost',
+        //user:     process.env.PG_USER     || 'postgres',
+        //password: process.env.PG_PASSWORD || '123456',
+        //database: process.env.PG_DATABASE || 'SA02',
+        //port:     process.env.PG_PORT     || 5532,
+        //url:      process.env.DATABASE_URL || 'jdbc:postgresql://localhost:5432/sa02',
+        pool: false
+        //ssl: true
+    }
 
 
-  /***************************************************************************
-  *                                                                          *
-  * More adapters: https://github.com/balderdashy/sails                      *
-  *                                                                          *
-  ***************************************************************************/
+    /***************************************************************************
+     *                                                                          *
+     * More adapters: https://github.com/balderdashy/sails                      *
+     *                                                                          *
+     ***************************************************************************/
 
 };
